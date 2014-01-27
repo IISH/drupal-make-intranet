624872c857d82f7e5b79f7b3de3996831af1f0c6
Lucien van Wouw <lwo@iisg.nl>
2014-01-27 21:10:02 +0100

    Create target folder for build

--- build.sh
+++ build.sh
@@ -4,10 +4,17 @@
 #
 # Build the package
 revision=$(git rev-parse HEAD)
-target=intranet.$revision
+target=target/intranet
 expect=$target.tar.gz
-echo "Prepare build for intranet.make and will call it $expect"
-drush make --tar intranet.make intranet.$revision
+
+echo "Build revision $revision for intranet.make and will call it $expect"
+
+# Remove a possibly corrupt build.
+if [ -f $target ] ; then
+	rm $target
+fi
+
+drush make --tar intranet.make ./$target
 rc=$?
 if [[ $rc != 0 ]] ; then
 	echo "Drush failed to build the package. Error: $rc."
