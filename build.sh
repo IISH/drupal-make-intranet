add93c26bf6fb43221a0fc0f08335a323d584401
Lucien van Wouw <lwo@iisg.nl>
2014-01-27 21:45:50 +0100

    Let build script clear target directory

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ expect=$target.tar.gz
 echo "Build revision $revision for intranet.make and will call it $expect"
 
 # Remove a possibly corrupt build.
-if [ -f $target ] ; then
-	rm $target
+if [ -d target ] ; then
+	rm -r $target
 fi
 
 drush make --tar intranet.make ./$target
 rc=$?
-if [[ $rc != 0 ]] ; then
+if [ $rc != 0 ] ; then
 	echo "Drush failed to build the package. Error: $rc."
 	exit -1
 fi
